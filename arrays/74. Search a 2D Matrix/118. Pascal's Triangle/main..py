e211dd505c340a10501861657aa0cdfba8d3c953
卐 <sinha036swastik@gmail.com>
2026-05-15 23:40:34 +0530

    Create main..py

---
+++ arrays/74. Search a 2D Matrix/118. Pascal's Triangle/main..py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        res = [[1]]
+        for i in range(numRows - 1):
+            temp = [0] + res[-1] + [0]
+            r = []
+            for j in range(len(res[-1]) + 1):
+                r.append(temp[j]+ temp[j+1])
+            res.append(r)
+        return res
